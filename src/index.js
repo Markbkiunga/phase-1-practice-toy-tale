3595d9c0a4b7e46eed996709ddb3c97150f50e56
Mark Brian <markbkiunga2017@gmail.com>
2024-07-13 10:42:31 +0300

    Can Fetch Andy's Toys

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const addBtn = document.querySelector('#new-toy-btn');
+  const toyFormContainer = document.querySelector('.container');
+  addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainer.style.display = 'block';
     } else {
-      toyFormContainer.style.display = "none";
+      toyFormContainer.style.display = 'none';
     }
   });
+});
+
+//Fetch Andy's toys
+let toysContainer = document.getElementById('toy-collection');
+window.addEventListener('load', () => {
+  fetch('http://localhost:3000/toys')
+    .then((response) => response.json())
+    .then((data) => {
+      for (let toy of data) {
+        let cardContainer = document.createElement('div');
+        cardContainer.classList.add('card');
+        toysContainer.appendChild(cardContainer);
+      }
+    });
 });
